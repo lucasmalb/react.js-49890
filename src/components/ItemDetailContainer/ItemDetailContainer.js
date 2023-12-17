f31393fe18cf74656d27b204e3bb45de863617ae
Lucas villalba <lucas.m.albrigi@gmail.com>
2023-12-17 11:53:46 -0300

    revision

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -2,32 +2,39 @@ import { useState, useEffect } from "react";
 import { getDoc, doc } from "firebase/firestore";
 import { db } from "../../config/Firebase";
 import ItemDetail from "../ItemDetail/ItemDetail";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-const ItemDetailContainer = () =>{
-    const [product, setProduct] = useState (null)
-    const [loading, setLoading] = useState (true)
-    const {itemId} = useParams()
+const ItemDetailContainer = () => {
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { itemId } = useParams();
 
-    useEffect(() => {
-        setLoading(true)
-        const docRef = doc(db, 'products', itemId)
-        getDoc(docRef)
-            .then(response =>{
-                const data = response.data()
-                const productAdapted = {id: response.id, ...data}
-                setProduct (productAdapted)
-            })
-        .catch((error) =>{console.error(error)
-        })
-    })
-        .finally (() =>{
-            setLoading(false)
-        },[itemId])
-    return(
-        <div>
-            <ItemDetail {...product}/>
-        </div>
-    )
-}
+  useEffect(() => {
+    setLoading(true);
+    const docRef = doc(db, "products", itemId);
+    getDoc(docRef)
+      .then((response) => {
+        const data = response.data();
+        const productAdapted = { id: response.id, ...data };
+        setProduct(productAdapted);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [itemId]);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        product && <ItemDetail {...product} />
+      )}
+    </div>
+  );
+};
+
 export default ItemDetailContainer;
